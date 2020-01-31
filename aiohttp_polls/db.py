fa242ee93c54d82d50a1f078f7a460596b285d23
eugeny-m <evgenii.maksimov@yahoo.com>
2020-02-01 00:44:44 +0300

    add poll, question, choice models and init_db script

---
+++ aiohttp_polls/db.py
@@ -0,0 +1,118 @@
+import aiopg.sa
+from sqlalchemy import (
+    MetaData, Table, Column, ForeignKey,
+    Integer, String, Date
+)
+
+
+__all__ = ['poll', 'question', 'choice']
+
+meta = MetaData()
+
+
+# Poll table description
+poll = Table(
+    'poll',
+    meta,
+
+    Column('id', Integer, primary_key=True),
+    Column('title', String(200), nullable=False),
+
+)
+
+
+# Question table description
+question = Table(
+    'question',
+    meta,
+
+    Column('id', Integer, primary_key=True),
+    Column('question_text', String(200), nullable=False),
+    Column(
+        'poll_id',
+        Integer,
+        ForeignKey('poll.id', ondelete='CASCADE'),
+        nullable=False,
+    ),
+)
+
+
+# Choice table description
+choice = Table(
+    'choice',
+    meta,
+
+    Column('id', Integer, primary_key=True),
+    Column('choice_text', String(200), nullable=False),
+    Column('votes', Integer, server_default="0", nullable=False),
+    Column(
+        'question_id',
+        Integer,
+        ForeignKey('question.id', ondelete='CASCADE'),
+        nullable=False,
+    ),
+)
+
+
+class RecordNotFound(Exception):
+    """Requested record in database was not found"""
+
+
+async def init_pg(app):
+    conf = app['config']['postgres']
+    engine = await aiopg.sa.create_engine(
+        database=conf['database'],
+        user=conf['user'],
+        password=conf['password'],
+        host=conf['host'],
+        port=conf['port'],
+        minsize=conf['minsize'],
+        maxsize=conf['maxsize'],
+    )
+    app['db'] = engine
+
+
+async def close_pg(app):
+    app['db'].close()
+    await app['db'].wait_closed()
+
+
+async def get_children(conn, parent, parent_field, child):
+    result = await conn.execute(
+        child.select()
+        .where(getattr(child.c, parent_field) == parent.id))
+    children = await result.fetchall()
+    return children
+
+
+async def get_poll_questions(conn, poll_id):
+    # get poll object
+    result = await conn.execute(
+        poll.select()
+        .where(poll.c.id == poll_id))
+    poll_record = await result.first()
+
+    if not poll_record:
+        msg = "Poll with id: {} does not exists"
+        raise RecordNotFound(msg.format(poll_id))
+
+    return_dict = {
+        'poll': poll_record,
+        'questions': []
+    }
+
+    # get poll questions
+    questions = await get_children(conn, poll_record, 'poll_id', question)
+
+    # get choices for questions
+    for q_record in questions:
+        choices = await get_children(conn, q_record, 'question_id', choice)
+        return_dict['questions'].append({
+            'question': q_record,
+            'choices': choices
+        })
+
+    return return_dict
+
+
+
---
+++ init_db.py
@@ -0,0 +1,101 @@
+from sqlalchemy import create_engine, MetaData
+
+from aiohttp_polls.db import poll, question, choice
+from aiohttp_polls.settings import BASE_DIR, get_config
+
+
+DSN = "postgresql://{user}:{password}@{host}:{port}/{database}"
+
+ADMIN_DB_URL = DSN.format(
+    user='postgres', password='postgres', database='postgres',
+    host='localhost', port=5432
+)
+
+admin_engine = create_engine(ADMIN_DB_URL, isolation_level='AUTOCOMMIT')
+
+USER_CONFIG_PATH = BASE_DIR / 'config' / 'polls.yaml'
+USER_CONFIG = get_config(['-c', USER_CONFIG_PATH.as_posix()])
+USER_DB_URL = DSN.format(**USER_CONFIG['postgres'])
+user_engine = create_engine(USER_DB_URL)
+
+TEST_CONFIG_PATH = BASE_DIR / 'config' / 'polls_test.yaml'
+TEST_CONFIG = get_config(['-c', TEST_CONFIG_PATH.as_posix()])
+TEST_DB_URL = DSN.format(**TEST_CONFIG['postgres'])
+test_engine = create_engine(TEST_DB_URL)
+
+
+def setup_db(config):
+
+    db_name = config['database']
+    db_user = config['user']
+    db_pass = config['password']
+
+    conn = admin_engine.connect()
+    conn.execute("DROP DATABASE IF EXISTS %s" % db_name)
+    conn.execute("DROP ROLE IF EXISTS %s" % db_user)
+    conn.execute("CREATE USER %s WITH PASSWORD '%s'" % (db_user, db_pass))
+    conn.execute("CREATE DATABASE %s ENCODING 'UTF8'" % db_name)
+    conn.execute("GRANT ALL PRIVILEGES ON DATABASE %s TO %s" %
+                 (db_name, db_user))
+    conn.close()
+
+
+def teardown_db(config):
+
+    db_name = config['database']
+    db_user = config['user']
+
+    conn = admin_engine.connect()
+    conn.execute("""
+      SELECT pg_terminate_backend(pg_stat_activity.pid)
+      FROM pg_stat_activity
+      WHERE pg_stat_activity.datname = '%s'
+        AND pid <> pg_backend_pid();""" % db_name)
+    conn.execute("DROP DATABASE IF EXISTS %s" % db_name)
+    conn.execute("DROP ROLE IF EXISTS %s" % db_user)
+    conn.close()
+
+
+def create_tables(engine=test_engine):
+    meta = MetaData()
+    meta.create_all(bind=engine, tables=[poll, question, choice])
+
+
+def drop_tables(engine=test_engine):
+    meta = MetaData()
+    meta.drop_all(bind=engine, tables=[poll, question, choice])
+
+
+def sample_data(engine=test_engine):
+    conn = engine.connect()
+    conn.execute(poll.insert(), [
+        {'title': 'The first poll'},
+        {'title': 'The second poll'},
+    ])
+    conn.execute(question.insert(), [
+        {'question_text': 'Poll 1 question 1', 'poll_id': 1},
+        {'question_text': 'Poll 1 question 2', 'poll_id': 1},
+        {'question_text': 'Poll 2 question 1', 'poll_id': 2},
+    ])
+    conn.execute(choice.insert(), [
+        {'choice_text': 'Choice 1 question 1 poll 1', 'question_id': 1},
+        {'choice_text': 'Choice 2 question 1 poll 1', 'question_id': 1},
+        {'choice_text': 'Choice 3 question 1 poll 1', 'question_id': 1},
+        {'choice_text': 'Choice 4 question 1 poll 1', 'question_id': 1},
+
+        {'choice_text': 'Choice 1 question 1 poll 1', 'question_id': 2},
+        {'choice_text': 'Choice 2 question 2 poll 1', 'question_id': 2},
+
+        {'choice_text': 'Choice 1 question 1 poll 2', 'question_id': 3},
+        {'choice_text': 'Choice 2 question 1 poll 2', 'question_id': 3},
+    ])
+    conn.close()
+
+
+if __name__ == '__main__':
+
+    setup_db(USER_CONFIG['postgres'])
+    create_tables(engine=user_engine)
+    sample_data(engine=user_engine)
+    # drop_tables(engine=user_engine)
+    # teardown_db(USER_CONFIG['postgres'])
