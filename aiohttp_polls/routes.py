649fd4df5d75ed15b24ca68c51340ed2027f36a9
eugeny-m <evgenii.maksimov@yahoo.com>
2020-02-01 03:09:02 +0300

    add index page name to routes

--- aiohttp_polls/routes.py
+++ aiohttp_polls/routes.py
@@ -8,7 +8,7 @@ PROJECT_ROOT = pathlib.Path(__file__).parent
 
 
 def setup_routes(app):
-    app.router.add_get('/', index)
+    app.router.add_get('/', index, name='index')
     app.router.add_get('/poll/{poll_id}', poll, name='poll')
     app.router.add_post('/poll/{poll_id}/vote', vote, name='vote')
     setup_static_routes(app)
