7de63e8ed7aa88ea434d543c1eead6eaa7fca64d
eugeny-m <evgenii.maksimov@yahoo.com>
2020-02-02 23:31:11 +0300

    add logout view and button to base header

--- aiohttp_polls/routes.py
+++ aiohttp_polls/routes.py
@@ -2,7 +2,7 @@
 import pathlib
 
 from .views import (index, poll, vote, registration_page, registration,
-    LoginView)
+    LoginView, logout)
 from . admin import (
     admin_index,
     ChoiceAdmin,
@@ -17,6 +17,7 @@ PROJECT_ROOT = pathlib.Path(__file__).parent
 def setup_routes(app):
     app.router.add_get('/', index, name='index')
     app.router.add_view('/login', LoginView, name='login')
+    app.router.add_view('/logout', logout, name='logout')
     app.router.add_get('/poll/{poll_id}', poll, name='poll')
     app.router.add_post('/poll/{poll_id}/vote', vote, name='vote')
     app.router.add_get('/admin', admin_index, name='admin')
